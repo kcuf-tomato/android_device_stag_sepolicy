8f703246acde10a4060d83f93c9ce3eaaf6b2751
Anushek Prasal <anushekprasal@gmail.com>
2019-04-05 16:38:53 +0530

    Init Stag...

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Stag
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/stag/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/stag/sepolicy/qcom/common \
+    device/stag/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
