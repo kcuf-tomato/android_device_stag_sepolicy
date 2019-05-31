9bcf9d85903cc298d3b9c23853cb36e4caf67708
vjspranav <pranavasri@live.in>
2019-05-31 16:24:19 +0200

    Init stag...

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Pixel Experience
+# inherit from Stag-OS
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/custom/sepolicy/common/public
+    device/stag/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/common/private
+    device/stag/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/custom/sepolicy/common/vendor
+    device/stag/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Pixel Experience
+# that inherit from Stag-OS
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/qcom/private
+    device/stag/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/custom/sepolicy/qcom/common \
-    device/custom/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/stag/sepolicy/qcom/common \
+    device/stag/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
